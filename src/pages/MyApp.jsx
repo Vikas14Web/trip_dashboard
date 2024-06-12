e5b2b5304654332b2d9b3152b628f88dfb8af305
Vikas Singh Chouhan <vikas-c@pluralsight.com>
2024-06-12 12:34:26 +0530

    1-trip-dashboard-assets

---
+++ src/pages/MyApp.jsx
@@ -0,0 +1,14 @@
+// import 'src/styles/globals.css';
+import Header from "../components/header/Header";
+import Home from "../components/home";
+
+const MyApp = ({  }) => {
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
+};
+
+export default MyApp;
